5afdb3790a9263cb7cdc93bd2bf7f3d0adfae198
Johanni Brea <jbrea@users.noreply.github.com>
2018-10-15 10:00:10 +0200

    add AllElasticArray

--- src/ElasticPDMats.jl
+++ src/ElasticPDMats.jl
@@ -1,71 +1,110 @@
 module ElasticPDMats
 import Base: size, getindex, setindex!, append!, show, view, deleteat!
 import LinearAlgebra: mul!, ldiv!
-using LinearAlgebra
+using LinearAlgebra, MacroTools
 using PDMats
 import PDMats: dim, Matrix, diag, pdadd!, *, \, inv, logdet, eigmax, eigmin, whiten!, unwhiten!, quad, quad!, invquad, invquad!, X_A_Xt, Xt_A_X, X_invA_Xt, Xt_invA_X
-export ElasticPDMat, ElasticSymmetricMatrix, ElasticCholesky, setcapacity!, setstepsize!
+export ElasticPDMat, AllElasticArray, ElasticSymmetricMatrix, ElasticCholesky, setcapacity!, setstepsize!
 
-mutable struct ElasticSymmetricMatrix{T} <: AbstractArray{T, 2}
-    N::Int64
-    capacity::Int64
-    stepsize::Int64
-    data::Array{T, 2}
+mutable struct AllElasticArray{T, N} <: AbstractArray{T, N}
+    dims::Tuple{Vararg{Int64, N}}
+    capacity::Tuple{Vararg{Int64, N}}
+    stepsize::Tuple{Vararg{Int64, N}}
+    data::Array{T, N}
+end
+function AllElasticArray(m::AbstractArray{T, N}; dims = size(m), 
+                         capacity = fill(10^2, N), 
+                         stepsize = fill(10^2, N)) where {T, N}
+    AllElasticArray(dims, Tuple(max.(dims, capacity)), Tuple(max.(dims, stepsize)), m)
+end
+AllElasticArray(N; capacity = tuple(fill(10^2, N)...), stepsize = tuple(fill(10^2, N)...)) = 
+AllElasticArray(zeros(capacity...), dims = tuple(fill(0, N)...), capacity = capacity,
+                    stepsize = stepsize)
+
+size(m::AllElasticArray) = m.dims
+for i in 2:3
+    vars = [Symbol(:i, j) for j in 1:i]
+    @eval begin
+        getindex(m::AllElasticArray{T, $i}, $(vars...)) where {T} = getindex(m.data, $(vars...))
+        setindex!(m::AllElasticArray{T, $i}, v::T, $(vars...)) where {T} = setindex!(m.data,v, $(vars...))
+        view(m::AllElasticArray{T, $i}, $(vars...)) where {T} = view(m.data, $(vars...))
+        view(m::AllElasticArray{T, $i}) where {T} = view(m, $([:(UnitRange(1, m.dims[$j])) for j in 1:i]...))
+        setdim!(m::AllElasticArray{T, $i}, v::Int, k::Int) where {T} = m.dims = tuple($([:(k == $j ? v : m.dims[$j]) for j in 1:i]...))
+    end
+end
+getindex(m::AllElasticArray{T, N}, i, j, k, l, I...) where {T, N} = getindex(m.data, i, j, k, l, I...)
+view(m::AllElasticArray{T, N}) where {T, N} = view(m, (UnitRange.(1, m.dims))...)
+setindex!(m::AllElasticArray{T, N}, I::Vararg{Int64, N}) where {T, N} = setindex!(m.data, I...)
+setdim!(m::AllElasticArray{T, N}, v::Int, k::Int) where {T, N} = m.dims = tuple([k == j ? v : m.dims[j] for j in 1:N]...)
+setdim!(m::AllElasticArray, v::Int, k::AbstractArray{Int, 1}) = for i in k setdim!(m, v, i) end
+function grow!(obj::AllElasticArray)
+    obj.capacity = obj.capacity .+ obj.stepsize
+    resize!(obj)
+end
+resize!(obj::AllElasticArray) = resize!(obj, obj.data)
+function resize!(obj::AllElasticArray, data::AbstractArray{T, N}) where {T, N}
+    tmp = zeros(T, obj.capacity...)
+    ind = CartesianIndices(Tuple(UnitRange.(1, obj.dims)))
+    copyto!(tmp, ind, data, ind)
+    obj.data = tmp
+    obj.capacity
+end
+
+mul!(Y::AbstractArray{T, 1}, M::AllElasticArray{T, 2}, V::AbstractArray{T, 1}) where {T} = mul!(Y, view(M), V)
+mul!(Y::AbstractArray{T, 2}, M::AllElasticArray{T, 2}, V::AbstractArray{T, 2}) where {T} = mul!(Y, view(M), V)
+mul!(Y::AbstractArray{T, 2}, M1::AllElasticArray{T, 2}, M2::AllElasticArray{T, 2}) where {T} = mul!(Y, view(M1), view(M2))
+
+struct ElasticSymmetricMatrix{T} <: AbstractArray{T, 2}
+    m::AllElasticArray{T, 2}
 end
 function ElasticSymmetricMatrix(m::AbstractArray{T, 2}; N = size(m, 1), capacity = 10^3, stepsize = 10^3) where {T}
     !issymmetric(m) && error("Data is not symmetric.")
     data = zeros(T, capacity, capacity)
     ind = CartesianIndices((1:N, 1:N))
     copyto!(data, ind, m, ind)
-    ElasticSymmetricMatrix(N, capacity, stepsize, data)
+    ElasticSymmetricMatrix(AllElasticArray((N, N), (capacity, capacity), (stepsize, stepsize), data))
 end
-ElasticSymmetricMatrix(; capacity = 10^3, stepsize = 10^3) = ElasticSymmetricMatrix(0, capacity, stepsize, zeros(capacity, capacity))
+ElasticSymmetricMatrix(; capacity = 10^3, stepsize = 10^3) = ElasticSymmetricMatrix(AllElasticArray((0, 0), (capacity, capacity), (stepsize, stepsize), zeros(capacity, capacity)))
 
-size(m::ElasticSymmetricMatrix) = (m.N, m.N)
-getindex(m::ElasticSymmetricMatrix, i::Integer, j::Integer) = m.data[i, j]
-function setindex!(m::ElasticSymmetricMatrix{T}, x::T, i::Integer, j::Integer) where T
-    setindex!(m.data, x, i, j)
-    i != j && setindex!(m.data, x, j, i)
-    m
+# from Lazy.jl
+macro forward(ex, fs)
+  @capture(ex, T_.field_) || error("Syntax: @forward T.x f, g, h")
+  T = esc(T)
+  fs = isexpr(fs, :tuple) ? map(esc, fs.args) : [esc(fs)]
+  :($([:($f(x::$T, args...) = (Base.@_inline_meta; $f(x.$field, args...)))
+       for f in fs]...);
+    nothing)
 end
-view(m::ElasticSymmetricMatrix, i, j) = view(m.data, i, j)
-view(m::ElasticSymmetricMatrix) = view(m, 1:m.N, 1:m.N)
-
-mul!(Y::AbstractArray{T, 1}, M::ElasticSymmetricMatrix{T}, V::AbstractArray{T, 1}) where {T} = mul!(Y, view(M), V)
-mul!(Y::AbstractArray{T, 2}, M::ElasticSymmetricMatrix{T}, V::AbstractArray{T, 2}) where {T} = mul!(Y, view(M), V)
-mul!(Y::AbstractArray{T, 2}, M1::ElasticSymmetricMatrix{T}, M2::ElasticSymmetricMatrix{T}) where {T} = mul!(Y, view(M1), view(M2))
-
-setnewdata!(obj::ElasticSymmetricMatrix, data) = obj.data = data
-resize!(obj::ElasticSymmetricMatrix) = resize!(obj, obj.data)
-function resize!(obj, data::AbstractArray{T, 2}) where {T}
-    tmp = zeros(T, obj.capacity, obj.capacity)
-    ind = CartesianIndices((1:obj.N, 1:obj.N))
-    copyto!(tmp, ind, data, ind)
-    setnewdata!(obj, tmp)
-    obj.capacity
-end
-
-append!(g::ElasticSymmetricMatrix{T}, data::Vector{T}) where {T} = append!(g, reshape(data, :, 1))
+@forward ElasticSymmetricMatrix.m size, getindex, setindex!, view, mul!, grow!, resize!, setnewdata!
+setdim!(e::ElasticSymmetricMatrix, N::Int) = setdim!(e.m, N, 1:2)
 function append!(g::ElasticSymmetricMatrix{T}, data::AbstractArray{T, 2}) where {T}
     n, m = size(data)
-    g.N + m > g.capacity && grow!(g)
-    for j in 1:m
-        for i in 1:min(g.N + j, n)
-            @inbounds g.data[i, j + g.N] = data[i, j]
-            @inbounds g.data[j + g.N, i] = data[i, j]
+    oldm = size(g, 1)
+    oldm + m > g.m.capacity[1] && grow!(g)
+    gd = g.m.data
+    @inbounds @simd for j in 1:m
+        for i in 1:min(oldm + j, n)
+            gd[i, j + oldm] = gd[j + oldm, i] = data[i, j]
         end
     end
-    g.N += m
+    setdim!(g, oldm + m) 
     g
 end
 function deleteat!(g::ElasticSymmetricMatrix, i::Int)
-    copyto!(g.data, CartesianIndices((1:g.N, i:g.N - 1)), 
-            g.data, CartesianIndices((1:g.N, i+1:g.N)))
-    copyto!(g.data, CartesianIndices((i:g.N-1, 1:g.N)), 
-            g.data, CartesianIndices((i+1:g.N, 1:g.N)))
-    g.N -= 1
+    N = size(g, 1)
+    gd = g.m.data
+    copyto!(gd, CartesianIndices((1:N, i:N - 1)), 
+            gd, CartesianIndices((1:N, i+1:N)))
+    copyto!(gd, CartesianIndices((i:N-1, 1:N)), 
+            gd, CartesianIndices((i+1:N, 1:N)))
+    setdim!(g, N - 1)
     g
 end
+function setcapacity!(x::ElasticSymmetricMatrix, c::Int)
+    x.m.capacity = (c, c)
+    resize!(x)
+end
+setstepsize!(x::ElasticSymmetricMatrix, c::Int) = x.m.stepsize = (c, c)
 
 mutable struct ElasticCholesky{T, A} <: Factorization{T}
     N::Int64
@@ -83,11 +122,11 @@ end
 ElasticCholesky(; capacity = 10^3, stepsize = 10^3) = ElasticCholesky(0, capacity, stepsize, Cholesky(zeros(capacity, capacity), 'U', LinearAlgebra.BlasInt(0)))
 
 
-function setcapacity!(x::Union{ElasticSymmetricMatrix, ElasticCholesky}, c::Int)
+function setcapacity!(x::ElasticCholesky, c::Int)
     x.capacity = c
     resize!(x)
 end
-setstepsize!(x::Union{ElasticSymmetricMatrix, ElasticCholesky}, c::Int) = x.stepsize = c
+setstepsize!(x::ElasticCholesky, c::Int) = x.stepsize = c
 
 view(c::ElasticCholesky, i, j) = Cholesky(view(c.c.factors, i, j), c.c.uplo, c.c.info)
 view(c::ElasticCholesky) = view(c, 1:c.N, 1:c.N)
@@ -97,14 +136,20 @@ size(c::ElasticCholesky, i::Int) = c.N
 
 ldiv!(c::ElasticCholesky, x) = ldiv!(view(c), x)
 
-function grow!(c::Union{ElasticCholesky, ElasticSymmetricMatrix})
+function grow!(c::ElasticCholesky)
     c.capacity += c.stepsize
     resize!(c)
 end
 resize!(c::ElasticCholesky) = resize!(c, c.c.factors)
-setnewdata!(c::ElasticCholesky, data) = c.c = Cholesky(data, 'U', LinearAlgebra.BlasInt(0))
+function resize!(obj::ElasticCholesky, data::AbstractArray{T, N}) where {T, N}
+    tmp = zeros(T, obj.capacity, obj.capacity)
+    ind = CartesianIndices((1:obj.N, 1:obj.N))
+    copyto!(tmp, ind, data, ind)
+    obj.c = Cholesky(tmp, 'U', LinearAlgebra.BlasInt(0))
+    obj.capacity
+end
 
-append!(c::ElasticCholesky{T, A}, data::Vector{T}) where {T, A} = append!(c, reshape(data, :, 1))
+append!(c::Union{ElasticCholesky{T, A}, ElasticSymmetricMatrix{T}}, data::Vector{T}) where {T, A} = append!(c, reshape(data, :, 1))
 function append!(c::ElasticCholesky{T,A}, data::A) where {T, A}
     n, m = size(data)
     c.N + m > c.capacity && grow!(c)
@@ -122,7 +167,7 @@ end
 function deleteat!(c::ElasticCholesky, i::Int)
     R = view(c.c.factors, i, i+1:c.N) * view(c.c.factors, i, i+1:c.N)' 
     R += view(c, i+1:c.N, i+1:c.N).U' * view(c, i+1:c.N, i+1:c.N).U
-    cholesky!(R)
+    cholesky!(Symmetric(R))
     copyto!(c.c.factors, CartesianIndices((1:i-1, i:c.N-1)),
             c.c.factors, CartesianIndices((1:i-1, i+1:c.N)))
     copyto!(c.c.factors, CartesianIndices((i:c.N-1, i:c.N-1)),
@@ -143,7 +188,7 @@ Creates an elastic positive definite matrix with initial `capacity = 10^3` and
 matrix and `chol` its cholesky decomposition. Use `append!` and `deleteat!` to
 change an ElasticPDMat.
 """
-ElasticPDMat(; kwargs...) = ElasticPDMat(ElasticSymmetricMatrix(; kwargs...), ElasticCholesky(kwargs...))
+ElasticPDMat(; kwargs...) = ElasticPDMat(ElasticSymmetricMatrix(; kwargs...), ElasticCholesky(; kwargs...))
 ElasticPDMat(m; kwargs...) = ElasticPDMat(m, cholesky(m); kwargs...)
 function ElasticPDMat(m, chol; kwargs...)
     ElasticPDMat(ElasticSymmetricMatrix(m; kwargs...),
@@ -174,7 +219,7 @@ function deleteat!(g::Union{ElasticSymmetricMatrix, ElasticCholesky, ElasticPDMa
     map(i -> deleteat!(g, i), sort(idxs, rev = true))
 end
 
-dim(a::ElasticPDMat) = a.mat.N
+dim(a::ElasticPDMat) = size(a.mat, 1)
 Base.Matrix(a::ElasticPDMat) = Matrix(view(a.mat))
 LinearAlgebra.diag(a::ElasticPDMat) = diag(view(a.mat))
 function pdadd!(r::Matrix, a::Matrix, gb::ElasticPDMat, c::Real)
@@ -194,11 +239,11 @@ function pdadd!(r::Matrix, a::Matrix, gb::ElasticPDMat, c::Real)
     return r
 end
 
-*(a::ElasticPDMat, c::Real) = ElasticPDMat(c * Matrix(a), capacity = a.mat.capacity, stepsize = a.mat.stepsize) 
+*(a::ElasticPDMat, c::Real) = ElasticPDMat(c * Matrix(a), capacity = a.chol.capacity, stepsize = a.chol.stepsize) 
 *(a::ElasticPDMat, x::AbstractArray) = a.mat * x 
 \(a::ElasticPDMat, x::AbstractArray) = a.chol \ x
 
-inv(a::ElasticPDMat) = ElasticPDMat(inv(a.chol), capacity = a.mat.capacity, stepsize = a.mat.stepsize) 
+inv(a::ElasticPDMat) = ElasticPDMat(inv(a.chol), capacity = a.chol.capacity, stepsize = a.chol.stepsize) 
 logdet(a::ElasticPDMat) = logdet(view(a.chol)) 
 eigmax(a::ElasticPDMat) = eigmax(view(a.mat))
 eigmin(a::ElasticPDMat) = eigmin(view(a.mat))
--- test/runtests.jl
+++ test/runtests.jl
@@ -19,8 +19,8 @@ end
 
     epdmat = ElasticPDMat(m[1:6, 1:6], capacity = 6, stepsize = 6)
     append!(epdmat, m[:, 7:10])
-    @test epdmat.mat.capacity == epdmat.chol.capacity == 
-        size(epdmat.mat.data, 1) == size(epdmat.chol.c.factors, 1) == 12
+    @test epdmat.mat.m.capacity[1] == epdmat.chol.capacity == 
+        size(epdmat.mat.m.data, 1) == size(epdmat.chol.c.factors, 1) == 12
     @test cholesky(m).U ≈ view(epdmat.chol).U
 end
 
@@ -43,15 +43,15 @@ end
     epdmat = ElasticPDMat(m, capacity = 100, stepsize = 50)
     epdmat2 = ElasticPDMat(m, cholesky(m), capacity = 100, stepsize = 50)
     epdmat3 = ElasticPDMat()
-    @test epdmat3.mat.capacity == epdmat3.chol.capacity == epdmat3.mat.stepsize == epdmat3.chol.stepsize == 10^3
-    @test epdmat.mat.data == epdmat2.mat.data
+    @test epdmat3.mat.m.capacity[1] == epdmat3.chol.capacity == epdmat3.mat.m.stepsize[1] == epdmat3.chol.stepsize == 10^3
+    @test epdmat.mat.m.data == epdmat2.mat.m.data
     @test epdmat.chol.c.factors == epdmat2.chol.c.factors
     @test size(epdmat.mat) == (10, 10)
     setstepsize!(epdmat, 100)
-    @test epdmat.mat.stepsize == epdmat.chol.stepsize == 100
+    @test epdmat.mat.m.stepsize[1] == epdmat.chol.stepsize == 100
     setcapacity!(epdmat, 20)
-    @test epdmat.mat.capacity == epdmat.chol.capacity == 20
-    @test size(epdmat.mat.data) == size(epdmat.chol.c.factors) == (20, 20)
+    @test epdmat.mat.m.capacity[1] == epdmat.chol.capacity == 20
+    @test size(epdmat.mat.m.data) == size(epdmat.chol.c.factors) == (20, 20)
     @test view(epdmat.mat)[:] == m[:]
     @test view(epdmat.chol).U == cholesky(m).U
 end
